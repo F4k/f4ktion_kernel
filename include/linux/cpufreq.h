248542c293f4184c893e70a20ba40b59871cbb00
f4k <tusemociones@gmail.com>
2013-12-12 16:38:02 -0300

    cpufreq: added SmartASSV2 CPU governor

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -426,6 +426,9 @@ extern struct cpufreq_governor cpufreq_gov_interactive;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_LIONHEART)
 extern struct cpufreq_governor cpufreq_gov_lionheart;
 #define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_lionheart)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_SMARTASS2)
+extern struct cpufreq_governor cpufreq_gov_smartass2;
+#define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_smartass2)
 #endif
 
 
