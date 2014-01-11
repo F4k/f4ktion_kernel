0ee7764811df3cf0dd096613cc79dcbf990938c6
f4k <tusemociones@gmail.com>
2014-01-11 19:02:37 -0300

    cpufreq: update again SAV2 and SAH3 to fit new low clock freqs.

--- drivers/cpufreq/cpufreq_smartass2.c
+++ drivers/cpufreq/cpufreq_smartass2.c
@@ -45,7 +45,7 @@
  * towards the ideal frequency and slower after it has passed it. Similarly,
  * lowering the frequency towards the ideal frequency is faster than below it.
  */
-#define DEFAULT_AWAKE_IDEAL_FREQ (594*1000)
+#define DEFAULT_AWAKE_IDEAL_FREQ (378*1000)
 static unsigned int awake_ideal_freq;
 
 /*
@@ -54,7 +54,7 @@ static unsigned int awake_ideal_freq;
  * that practically when sleep_ideal_freq==0 the awake_ideal_freq is used
  * also when suspended).
  */
-#define DEFAULT_SLEEP_IDEAL_FREQ (324*1000)
+#define DEFAULT_SLEEP_IDEAL_FREQ (270*1000)
 static unsigned int sleep_ideal_freq;
 
 /*
@@ -62,7 +62,7 @@ static unsigned int sleep_ideal_freq;
  * Zero disables and causes to always jump straight to max frequency.
  * When below the ideal freqeuncy we always ramp up to the ideal freq.
  */
-#define DEFAULT_RAMP_UP_STEP (486*1000)
+#define DEFAULT_RAMP_UP_STEP (378*1000)
 static unsigned int ramp_up_step;
 
 /*
@@ -70,7 +70,7 @@ static unsigned int ramp_up_step;
  * Zero disables and will calculate ramp down according to load heuristic.
  * When above the ideal freqeuncy we always ramp down to the ideal freq.
  */
-#define DEFAULT_RAMP_DOWN_STEP (324*1000)
+#define DEFAULT_RAMP_DOWN_STEP (270*1000)
 static unsigned int ramp_down_step;
 
 /*
