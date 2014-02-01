19e0986f2540db0dfbe21fb7472b58648b98292c
f4k <tusemociones@gmail.com>
2014-02-01 18:23:05 -0300

    sbuild: update paths, remove build as JB variant (JB variant has now his own branch), print kernel name and size.

--- sbuild.sh
+++ sbuild.sh
@@ -19,11 +19,6 @@ case "$1" in
 esac
 
 BASE_F4K_VER=$BASE_F4K_VER-$VARIANT
-
-if [ "$2" = "jb" ] ; then
-	BASE_F4K_VER=$BASE_F4K_VER"-JB"
-fi
-
 F4K_VER=$BASE_F4K_VER$VER
 
 export LOCALVERSION="-"`echo $F4K_VER`
@@ -42,13 +37,12 @@ DATE_START=$(date +"%s")
 
 make VARIANT_DEFCONFIG=msm8930_serrano_$VARIANT"_defconfig" SELINUX_DEFCONFIG=selinux_defconfig SELINUX_LOG_DEFCONFIG=selinux_log_defconfig f4ktion_defconfig
 
-HOME_DIR=/home/f4k/kernels
-INIT_DIR=$HOME_DIR/ramdisks/$VARIANT
-MODULES_DIR=$HOME_DIR/filesdir/$VARIANT/lib/modules
+INIT_DIR=../ramdisks
+MODULES_DIR=../filesdir/$VARIANT/lib/modules
 KERNEL_DIR=`pwd`
-OUTPUT_DIR=$HOME_DIR/output/
-CWM_DIR=$HOME_DIR/filesdir/cwm
-CWM_ANY_DIR=$HOME_DIR/filesdir/cwm_any/
+OUTPUT_DIR=../output/$VARIANT
+CWM_DIR=../filesdir/cwm
+CWM_ANY_DIR=../filesdir/cwm_any
 
 echo
 echo "Removing old kernels files"
@@ -62,9 +56,13 @@ fi
 
 echo
 echo "Removing old modules"
-rm `find $KERNEL_DIR -name '*.ko'`
-rm `echo $MODULES_DIR"/*"`
-rm `echo $CWM_DIR/system/lib/modules/"*.ko"`
+if [ -e $KERNEL_DIR/block/test-iosched.ko ]; then
+	rm `find $KERNEL_DIR -name '*.ko'`
+	rm `echo $MODULES_DIR"/*"`
+	rm `echo $CWM_DIR/system/lib/modules/"*.ko"`
+else
+	echo "No modules found"
+fi
 
 echo
 echo "LOCALVERSION="$LOCALVERSION
@@ -77,39 +75,32 @@ echo "OUTPUT_DIR="$OUTPUT_DIR
 echo "CWM_DIR="$CWM_DIR
 echo "CWN_ANY_DIR="$CWM_ANY_DIR
 
-echo
-if [ "$2" = "jb" ] ; then
-        cd $HOME_DIR/ramdisks/ && git checkout jb-4.3
-fi
-
-cd $KERNEL_DIR
-
 make -j4 > /dev/null
 
 echo
 find $KERNEL_DIR -name '*.ko' -exec cp -v {} $MODULES_DIR \;
 find $MODULES_DIR -name '*.ko' -exec cp -v {} $CWM_DIR"/system/lib/modules/" \;
-cd $KERNEL_DIR
 
 echo
 if [ -e $KERNEL_DIR/arch/arm/boot/zImage ]; then
-	cp arch/arm/boot/zImage $CWM_ANY_DIR/
-	cd $CWM_ANY_DIR/
-	echo "Make boot.img"
-	./mkbootfs $INIT_DIR| gzip > $CWM_ANY_DIR/ramdisk.gz
-	./mkbootimg --cmdline 'console = null androidboot.hardware=qcom user_debug=31 zcache' --kernel $CWM_ANY_DIR/zImage --ramdisk $CWM_ANY_DIR/ramdisk.gz --base 0x80200000 --pagesize 2048 --ramdisk_offset 0x02000000 --output $CWM_DIR/boot.img
-
-	cd $CWM_DIR
+	cp arch/arm/boot/zImage $CWM_ANY_DIR
+	cd $INIT_DIR
+	./mkbootfs $VARIANT| gzip > $CWM_ANY_DIR/ramdisk.gz
+	cd $CWM_ANY_DIR
+	./mkbootimg --cmdline 'console = null androidboot.hardware=qcom user_debug=31 zcache' --kernel zImage --ramdisk ramdisk.gz --base 0x80200000 --pagesize 2048 --ramdisk_offset 0x02000000 --output ../cwm/boot.img
+	echo
+	echo "Make zip package"
+	cd ../cwm
 	zip -r `echo $F4K_VER`.zip *
-	mv  `echo $F4K_VER`.zip $OUTPUT_DIR/$VARIANT
+	mv  `echo $F4K_VER`.zip ../$OUTPUT_DIR
+	cd ../$OUTPUT_DIR
+	echo
+	FILE_NAME=$F4K_VER.zip
+	FILE_SIZE=$(stat -c%s "$FILE_NAME")
+	echo "$F4K_VER.zip size is $FILE_SIZE bytes."
 else
 	echo "KERNEL DID NOT BUILD! no zImage exist"
 fi;
-
-echo
-if [ "$2" = "jb" ] ; then
-        cd $HOME_DIR/ramdisks/ && git checkout kk-4.4
-fi
 
 cd $KERNEL_DIR
 
