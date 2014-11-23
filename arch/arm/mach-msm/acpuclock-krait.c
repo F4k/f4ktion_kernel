3eab0298ce4c5d75839b48d635ccbb7fb4fb4c19
f4k <tusemociones@gmail.com>
2014-11-23 20:47:54 -0300

    mach_msm: added CPU Overclock (OC) and Underclock (UC) support.

--- arch/arm/mach-msm/acpuclock-krait.c
+++ arch/arm/mach-msm/acpuclock-krait.c
@@ -54,6 +54,20 @@ int speed_bin;
 int pvs_bin;
 #endif
 
+#ifdef CONFIG_CPU_OVERCLOCK
+#ifdef CONFIG_CPU_UNDERCLOCK
+#define FREQ_TABLE_SIZE    44
+#else
+#define FREQ_TABLE_SIZE    39
+#endif
+#else
+#ifdef CONFIG_CPU_UNDERCLOCK
+#define FREQ_TABLE_SIZE    39
+#else
+#define FREQ_TABLE_SIZE    35
+#endif
+#endif
+
 static DEFINE_MUTEX(driver_lock);
 static DEFINE_SPINLOCK(l2_lock);
 
@@ -994,7 +1008,7 @@ ssize_t acpuclk_set_vdd(char *buf)
 }
 
 #ifdef CONFIG_CPU_FREQ_MSM
-static struct cpufreq_frequency_table freq_table[NR_CPUS][35];
+static struct cpufreq_frequency_table freq_table[NR_CPUS][FREQ_TABLE_SIZE];
 extern int console_batt_stat;
 static void __init cpufreq_table_init(void)
 {
