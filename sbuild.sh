0a05f2cf898b736312c1757d4d2808aa85ddb7c5
f4k <tusemociones@gmail.com>
2015-02-22 15:34:57 -0300

    sbuild: update a few things

--- sbuild.sh
+++ sbuild.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-BASE_F4K_VER="f4ktion_2.0.0"
+BASE_F4K_VER="f4ktion_kk_2.0.0"
 
 case "$1" in
         eur_3g)
@@ -27,7 +27,7 @@ BASE_F4K_VER=$BASE_F4K_VER-$VARIANT
 F4K_VER=$BASE_F4K_VER$VER
 
 export LOCALVERSION="-"`echo $F4K_VER`
-export CROSS_COMPILE=/home/f4k/toolchains/arm-cortex_a15-linux-gnueabihf-linaro_4.9.3-2014.11/bin/arm-cortex_a15-linux-gnueabihf-
+export CROSS_COMPILE=/home/f4k/toolchains/arm-cortex_a15-linux-gnueabihf-linaro_4.9.3-2015.01/bin/arm-cortex_a15-linux-gnueabihf-
 export ARCH=arm
 export KBUILD_BUILD_USER=f4k
 export KBUILD_BUILD_HOST="mint17.1x64"
@@ -39,11 +39,11 @@ DATE_START=$(date +"%s")
 
 make VARIANT_DEFCONFIG=msm8930_serrano_$VARIANT"_defconfig" SELINUX_DEFCONFIG=selinux_defconfig f4ktion_defconfig
 
-INIT_DIR=../ramdisks_lp
+INIT_DIR=../ramdisks
 MODULES_DIR=../filesdir/$VARIANT/lib/modules
 KERNEL_DIR=`pwd`
 OUTPUT_DIR=../output/$VARIANT
-CWM_DIR=../filesdir/cwm_lp
+CWM_DIR=../filesdir/cwm
 CWM_ANY_DIR=../filesdir/cwm_any
 
 echo
@@ -73,7 +73,7 @@ echo "OUTPUT_DIR="$OUTPUT_DIR
 echo "CWM_DIR="$CWM_DIR
 echo "CWN_ANY_DIR="$CWM_ANY_DIR
 
-make -j16 > /dev/null
+make -j8 > /dev/null
 
 echo
 find $KERNEL_DIR -name '*.ko' -exec cp -v {} $MODULES_DIR \;
@@ -85,10 +85,10 @@ if [ -e $KERNEL_DIR/arch/arm/boot/zImage ]; then
 	cd $INIT_DIR
 	./mkbootfs $VARIANT| gzip > $CWM_ANY_DIR/ramdisk.gz
 	cd $CWM_ANY_DIR
-	./mkbootimg --cmdline 'console = null androidboot.hardware=qcom user_debug=31 androidboot.selinux=permissive' --kernel zImage --ramdisk ramdisk.gz --base 0x80200000 --pagesize 2048 --ramdisk_offset 0x02000000 --output ../cwm_lp/boot.img
+	./mkbootimg --cmdline 'console = null androidboot.hardware=qcom user_debug=31 androidboot.selinux=permissive' --kernel zImage --ramdisk ramdisk.gz --base 0x80200000 --pagesize 2048 --ramdisk_offset 0x02000000 --output ../cwm/boot.img
 	echo
 	echo "Make zip package"
-	cd ../cwm_lp
+	cd ../cwm
 	zip -r `echo $F4K_VER`.zip *
 	mv  `echo $F4K_VER`.zip ../$OUTPUT_DIR
 	cd ../$OUTPUT_DIR
